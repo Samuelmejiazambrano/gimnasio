6e80590daa0a845106614c986abd9e8e626a5ec2
samuelmejia <samuelmejiaz2005@gmail.com>
2024-05-02 12:26:49 -0500

    Actualización:nuevo

--- qasar3/qasar1/vite-project/src/components/Inventario.vue
+++ qasar3/qasar1/vite-project/src/components/Inventario.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="rrr">
-    <h2 class="title">Hola Mundo</h2>
+    <h2 class="title">Lista de Inventario</h2>
     <div class="q-pa-md">
       <div class="q-pa-md q-gutter-sm">
         <div style="width: 120vh">
